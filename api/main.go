8d0ed8600d9ae6b2220559a9ad0b061884d22550
Takuya <yky2800.rthm.55tn@gmail.com>
2021-03-14 17:37:50 +0900

    fix import module & db connect

--- api/main.go
+++ api/main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	// "net/http"
+	"net/http"
 
 	"api/model"
-
-	"github.com/DAdDY0055/go_react/api/model"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
@@ -13,7 +11,7 @@ import (
 )
 
 func main() {
-	db, err = gorm.Open("mysql", "user:zaquser@tcp(localhost:3360)/todo?charset=utf8&parseTime=True&loc=Local")
+	db, err := gorm.Open("mysql", "user:zaquser@tcp(127.0.0.1:33060)/todo?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		panic("[main] db open error")
 	}
